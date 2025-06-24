d5e249c749c44fc8a19ec9f98c952fd4ce3fca8a
Edrick <144285943+EdrickOliveira@users.noreply.github.com>
2025-06-24 20:29:18 -0300

    style: identation standardization

--- 1º Trimestre/GPIO/Lógica Combinacional - Zucco/Nucelo G0/exercicio3_e_6.c
+++ 1º Trimestre/GPIO/Lógica Combinacional - Zucco/Nucelo G0/exercicio3_e_6.c
@@ -32,59 +32,58 @@ int main(void)
 {
 
 	// Configurando GPIOs
-
+	
 	RCC->IOPENR=0x0000003F; //habilita clock das portas
-
+	
 	GPIOB->MODER&=~(GPIO_MODER_MODER3|GPIO_MODER_MODER4|GPIO_MODER_MODER5); //entrada
-
+	
 	GPIOB->MODER&=~(GPIO_MODER_MODER8|GPIO_MODER_MODER9|GPIO_MODER_MODER10);
 	GPIOB->MODER|=(GPIO_MODER_MODER8_0|GPIO_MODER_MODER9_0|GPIO_MODER_MODER10_0); //saída
-
+	
 	//pull-down
 	GPIOB->PUPDR |=(GPIO_PUPDR_PUPDR8_0|GPIO_PUPDR_PUPDR9_0|GPIO_PUPDR_PUPDR10_0); //leds
 	GPIOB->PUPDR &=~(GPIO_PUPDR_PUPDR8|GPIO_PUPDR_PUPDR9|GPIO_PUPDR_PUPDR10);
-
+	
 	GPIOB->PUPDR |=(GPIO_PUPDR_PUPDR3_0|GPIO_PUPDR_PUPDR4_0|GPIO_PUPDR_PUPDR5_0); //botoes
 	GPIOB->PUPDR &=~(GPIO_PUPDR_PUPDR3|GPIO_PUPDR_PUPDR4|GPIO_PUPDR_PUPDR5);
 
-  /* Infinite loop */
-  while (1)
-  {
-botoes=GPIOB->IDR & (GPIO_IDR_IDR_5|GPIO_IDR_IDR_4|GPIO_IDR_IDR_3);
-botoes=botoes>>3;
-
-switch(botoes){
-case 0:
-	GPIOB->ODR&=~(GPIO_ODR_ODR_8|GPIO_ODR_ODR_9|GPIO_ODR_ODR_10);
-	break;
-case 1:
-	GPIOB->ODR|=GPIO_ODR_ODR_10;
-	GPIOB->ODR&=~(GPIO_ODR_ODR_8|GPIO_ODR_ODR_9);
-	break;
-case 2:
-	GPIOB->ODR|=GPIO_ODR_ODR_9;
-	GPIOB->ODR&=~(GPIO_ODR_ODR_8|GPIO_ODR_ODR_10);
-		break;
-case 3:
-	GPIOB->ODR|=(GPIO_ODR_ODR_9|GPIO_ODR_ODR_10);
-	GPIOB->ODR&=~(GPIO_ODR_ODR_8);
-	break;
-case 4:
-	GPIOB->ODR|=(GPIO_ODR_ODR_8);
-	GPIOB->ODR&=~(GPIO_ODR_ODR_9|GPIO_ODR_ODR_10);
-	break;
-case 5:
-	GPIOB->ODR|=(GPIO_ODR_ODR_8|GPIO_ODR_ODR_10);
-	GPIOB->ODR&=~(GPIO_ODR_ODR_9);
-	break;
-case 6:
-	GPIOB->ODR|=(GPIO_ODR_ODR_8|GPIO_ODR_ODR_9);
-	GPIOB->ODR&=~(GPIO_ODR_ODR_10);
-	break;
-case 7:
-	GPIOB->ODR|=(GPIO_ODR_ODR_8|GPIO_ODR_ODR_9|GPIO_ODR_ODR_10);
-	break;
+	/* Infinite loop */
+	while (1)
+	{
+		botoes=GPIOB->IDR & (GPIO_IDR_IDR_5|GPIO_IDR_IDR_4|GPIO_IDR_IDR_3);
+		botoes=botoes>>3;
+	
+		switch(botoes){
+		case 0:
+			GPIOB->ODR&=~(GPIO_ODR_ODR_8|GPIO_ODR_ODR_9|GPIO_ODR_ODR_10);
+			break;
+		case 1:
+			GPIOB->ODR|=GPIO_ODR_ODR_10;
+			GPIOB->ODR&=~(GPIO_ODR_ODR_8|GPIO_ODR_ODR_9);
+			break;
+		case 2:
+			GPIOB->ODR|=GPIO_ODR_ODR_9;
+			GPIOB->ODR&=~(GPIO_ODR_ODR_8|GPIO_ODR_ODR_10);
+			break;
+		case 3:
+			GPIOB->ODR|=(GPIO_ODR_ODR_9|GPIO_ODR_ODR_10);
+			GPIOB->ODR&=~(GPIO_ODR_ODR_8);
+			break;
+		case 4:
+			GPIOB->ODR|=(GPIO_ODR_ODR_8);
+			GPIOB->ODR&=~(GPIO_ODR_ODR_9|GPIO_ODR_ODR_10);
+			break;
+		case 5:
+			GPIOB->ODR|=(GPIO_ODR_ODR_8|GPIO_ODR_ODR_10);
+			GPIOB->ODR&=~(GPIO_ODR_ODR_9);
+			break;
+		case 6:
+			GPIOB->ODR|=(GPIO_ODR_ODR_8|GPIO_ODR_ODR_9);
+			GPIOB->ODR&=~(GPIO_ODR_ODR_10);
+			break;
+		case 7:
+			GPIOB->ODR|=(GPIO_ODR_ODR_8|GPIO_ODR_ODR_9|GPIO_ODR_ODR_10);
+			break;
+		}
+	}
 }
-  }
-}
-
